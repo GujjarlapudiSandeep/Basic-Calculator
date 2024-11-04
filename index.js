2f04c733afb481b3948ea6d858625bb2be04ff07
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-11-04 12:42:30 +0530

    added HTML,CSS,JS

---
+++ index.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+  const display = document.getElementById("display");
+  display.value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+  const display = document.getElementById("display");
+  try {
+    // Evaluate the expression
+    display.value = eval(display.value);
+  } catch (error) {
+    alert("Invalid calculation!");
+    clearDisplay();
+  }
+}
